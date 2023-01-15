1ff2cf87a07240a80cb7cf8405664353fc87a667
DashaMarchenko <you@example.com>
2023-01-15 19:54:58 +0300

    java

---
+++ index.js
@@ -0,0 +1,69 @@
+
+
+const str = "asd"
+// console.log(str.toLocaleLowerCase());
+// console.log(str.toUpperCase());
+
+// console.log(str.repeat(7))
+
+// console.log(str.toLocaleLowerCase() .includes('vet'))
+
+// const str2 = "RICGS"
+// console.log(str.concat(str2, 'asdafas', 1234));
+
+// console.log(str.startsWith('pRiV'));
+// console.log(str.endsWith('!'))
+// console.log(str.replaceAll('p', '#'))
+
+//  console.log(str.charAt(5))
+// console.log(str.indexOf('p'))
+// console.log(str.lastIndexOf("p"))
+// console.log(str.slice(2, 5))
+// console.log(str.substr(2, 2))
+
+// const inner = prompt()
+// console.log(inner.toLocaleUpperCase())
+// const mass = [4, 'chrom', null, {a:5}, [0,1,2,3,89]]
+// console.log(mass[4][4]);
+// const mass = [1,2,3,4,5,6,98,100]
+// const mass2 = [1,24,65]
+// console.log(mass.concat(mass2, 12395));
+// const str2 = 'qwerty asdsfa zxclfds'
+// const massstr2 = str2.split(' ')
+// console.log(massstr2)
+
+// const strmass3 = mass3.join('&')
+// console.log(strmass3);
+// console.log(mass4.reverse());
+// console.log(mass4.includes(6));
+// const mass4 = [0,9,8,7,6]
+// mass4.push(1,4,645,1234)
+// console.log(mass4);
+// const pop = mass4.pop()
+// console.log(mass4);
+// console.log(pop);
+// const mass5 =[3,14,543,12]
+// mass5.shift(3,14,543)
+// console.log(mass5);
+// console.log(mass6.sort((a,b) => a - b));
+// const mass6 = [123,4,2153,523,9124,529523]
+
+// const mass3 = [0,1,2,3,4,5,6,7,8,9]
+// console.log(mass3.slice(4,7));
+// console.log(mass3. splice(4, 3));
+// console.log('-------')
+//     console.log(mass3)
+//         console.log(mass3)
+// console.log(mass3.splice(4,3, 89, 213));
+// console.log(mass3);
+
+    const myName = prompt()
+    console.log(myName);
+    const arrName = myName. split(' ')
+    console.log(arrName);
+    arrName[0] = arrName[0].tolowerCase()
+    arrName[1] = arrName[1].toupperCase()
+    console.log(arrName);
+    const newMyname = arrName. join(' ')
+    console.log(newMyname);
+
