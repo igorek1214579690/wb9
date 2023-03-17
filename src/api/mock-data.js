166145b2f5b8bc2f9f4e9663b6374a63c5a89f93
unknown <lenkuai3@gmail.com>
2023-03-17 19:41:52 +0300

    header

---
+++ src/api/mock-data.js
@@ -0,0 +1,5 @@
+export const user = {
+    firstName:  'Igor',
+    secondName:  'Gorbach',
+    ava: 'https://images.theconversation.com/files/500899/original/file-20221214-461-22jr1l.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop'
+}
