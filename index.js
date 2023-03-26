8038f239f5729edd57a551d02b015d482def9321
VladiislavVolkov <kali.laskaa@gmail.com>
2023-03-26 17:07:40 +0300

    local-storage

--- index.js
+++ index.js
@@ -1,142 +1,52 @@
-const wrapper = document.querySelector('.wrapper')
+window.onload = load
+let storage = window.localStorage
+// console.log(storage);
+// storage.setItem('maikl', 'ivanova@')
+
+// let name = storage.getItem('maikl')
+// console.log(name);
+// storage.setItem ('city', 'Piter')
+
+//  let city = storage.getItem('city')
+//  console.log(storage.city);
+// storage.city = 'New york'
+// console.log(storage.city);
+
+// console.log(storage['maikl'])
+
+// let time = new Date()
+// storage.setItem('time', time)
+// console.log(time);
+
+function load(){
+    let firstName = document.getElementById('firstName')
+    let secondName = document.getElementById('secondName')
+    let email= document.getElementById('email')
+    let info = document.getElementById('info')
+    let rememberInfo = document.getElementById('rememberInfo')
+    // console.log(storage);
 
 
-const btn = document.getElementById('add-products')
-
-
-const products = 
-    [
-        {
-        "id": 1,
-        "title": "iPhone 9",
-        "description": "An apple mobile which is nothing like apple",
-        "price": 549,
-        "discountPercentage": 12.96,
-        "rating": 4.69,
-        "stock": 94,
-        "brand": "Apple",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/1/thumbnail.jpg",
-        "images": [
-        "https://i.dummyjson.com/data/products/1/1.jpg",
-        "https://i.dummyjson.com/data/products/1/2.jpg",
-        "https://i.dummyjson.com/data/products/1/3.jpg",
-        "https://i.dummyjson.com/data/products/1/4.jpg",
-        "https://i.dummyjson.com/data/products/1/thumbnail.jpg"
-        ]
-        },
-        {
-        "id": 2,
-        "title": "iPhone X",
-        "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-        "price": 899,
-        "discountPercentage": 17.94,
-        "rating": 4.44,
-        "stock": 34,
-        "brand": "Apple",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/2/thumbnail.jpg",
-        "images": [
-        "https://i.dummyjson.com/data/products/2/1.jpg",
-        "https://i.dummyjson.com/data/products/2/2.jpg",
-        "https://i.dummyjson.com/data/products/2/3.jpg",
-        "https://i.dummyjson.com/data/products/2/thumbnail.jpg"
-        ]
-        },
-        {
-        "id": 3,
-        "title": "Samsung Universe 9",
-        "description": "Samsung's new variant which goes beyond Galaxy to the Universe",
-        "price": 1249,
-        "discountPercentage": 15.46,
-        "rating": 4.09,
-        "stock": 36,
-        "brand": "Samsung",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/3/thumbnail.jpg",
-        "images": [
-        "https://i.dummyjson.com/data/products/3/1.jpg"
-        ]
-        },
-        {
-        "id": 4,
-        "title": "OPPOF19",
-        "description": "OPPO F19 is officially announced on April 2021.",
-        "price": 280,
-        "discountPercentage": 17.91,
-        "rating": 4.3,
-        "stock": 123,
-        "brand": "OPPO",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/4/thumbnail.jpg",
-        "images": [
-        "https://i.dummyjson.com/data/products/4/1.jpg",
-        "https://i.dummyjson.com/data/products/4/2.jpg",
-        "https://i.dummyjson.com/data/products/4/3.jpg",
-        "https://i.dummyjson.com/data/products/4/4.jpg",
-        "https://i.dummyjson.com/data/products/4/thumbnail.jpg"
-        ]
-        },
-        {
-        "id": 5,
-        "title": "Huawei P30",
-        "description": "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
-        "price": 499,
-        "discountPercentage": 10.58,
-        "rating": 4.09,
-        "stock": 32,
-        "brand": "Huawei",
-        "category": "smartphones",
-        "thumbnail": "https://i.dummyjson.com/data/products/5/thumbnail.jpg",
-        "images": [
-        "https://i.dummyjson.com/data/products/5/1.jpg",
-        "https://i.dummyjson.com/data/products/5/2.jpg",
-        "https://i.dummyjson.com/data/products/5/3.jpg"
-        ]
-        }
+    info.addEventListener('click', sub)
+    rememberInfo.addEventListener('click', print)
     
-    ]
-
-    btn.onclick = () => {
-        for (let i = 0; i < products.length; i++) {
-        const card = document.createElement('div')
-        card.classList.add('card')
-            
-        const title = document.createElement('h3') 
-        const thumbnail = document.createElement('img') 
-        const description = document.createElement('p') 
-        const price = document.createElement('p') 
-        const discountPercentage = document.createElement('p') 
-        const rating = document.createElement('p') 
-        const stock = document.createElement('p') 
-        const brand= document.createElement('p') 
-        const category = document.createElement('p') 
+}
 
 
-        thumbnail.src = products[i].thumbnail
-        thumbnail.innerHTML = products[i].thumbnail
-        title.innerHTML = products[i].title 
-        description.innerHTML = products[i].description
-        price.innerHTML = ` ${products[i].price}`
-        discountPercentage.innerHTML = `${products[i].discountPercentage}%`
-        rating.innerHTML = `rating is ${products[i].rating}`
-        brand.innerHTML =`brand "${products[i].brand}"`
-        category.innerHTML = `category "${products[i].category}"`
-       stock.innerHTML= `stock "${products[i].stock}"`
+function sub() {
+        let n1 = name1.value
+        let n2 = name2.value
+        let e = email.value
+        // console.log(n1 + n2 + email);
+        storage.setItem('firstName', n1)
+        storage.setItem('secondName', n2)
+        storage.setItem('email', e)
 
-        card.append
-        (thumbnail,
-            title,
-            description,
-            price,
-            rating,
-            brand,
-            category,
-            stock,
-            discountPercentage)
-       wrapper.append(card)
-        }
+
+
     }
 
-
-
+    function print(){
+        user.innerHTML = 'info about user' + storage.firstName + '' + storage.secondName + 'mail' +
+        storage.mail
+    }
